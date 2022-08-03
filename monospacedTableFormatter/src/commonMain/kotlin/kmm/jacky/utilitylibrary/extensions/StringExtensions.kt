dc6d71d3b4dcf4aa6616af132e4de6aacbe76a84
Jacky Tay <chianghau86@hotmail.com>
2022-08-03 22:22:13 +1200

    Change string repeat builder to use StringBuilder instead

--- monospacedTableFormatter/src/commonMain/kotlin/kmm/jacky/utilitylibrary/extensions/StringExtensions.kt
+++ monospacedTableFormatter/src/commonMain/kotlin/kmm/jacky/utilitylibrary/extensions/StringExtensions.kt
@@ -1,8 +1,8 @@
 package kmm.jacky.utilitylibrary.extensions
 
 import kmm.jacky.utilitylibrary.enums.Alignment
-import kmm.jacky.utilitylibrary.enums.LineWrap
 import kmm.jacky.utilitylibrary.models.column.Cell
+import kotlin.math.ceil
 
 val String.center: Cell
     get() = Cell(this, alignment = Alignment.Center)
@@ -15,7 +15,12 @@ fun String.span(span: Int): Cell = Cell(this, span = span)
 fun String.align(alignment: Alignment): Cell = Cell(this, alignment = alignment)
 
 fun String.buildRepeat(width: Int): String {
-    return (0..(width / length)).joinToString("") { this }.take(width)
+    return StringBuilder().let { builder ->
+        repeat(ceil(width.toDouble() / length).toInt()) {
+            builder.append(this)
+        }
+        builder.toString()
+    }.take(width)
 }
 
 internal fun String.findAllPunctuations(): List<MatchResult> {
