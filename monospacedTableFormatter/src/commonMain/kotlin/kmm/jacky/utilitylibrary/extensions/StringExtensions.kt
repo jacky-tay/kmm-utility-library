2f99cebe8dea588ffe5ee918d95fba1337ee9a55
Jacky Tay <chianghau86@hotmail.com>
2022-08-03 22:14:36 +1200

    Update LineWrap policy

--- monospacedTableFormatter/src/commonMain/kotlin/kmm/jacky/utilitylibrary/extensions/StringExtensions.kt
+++ monospacedTableFormatter/src/commonMain/kotlin/kmm/jacky/utilitylibrary/extensions/StringExtensions.kt
@@ -18,50 +18,6 @@ fun String.buildRepeat(width: Int): String {
     return (0..(width / length)).joinToString("") { this }.take(width)
 }
 
-internal fun String.process(
-    pos: Int,
-    boundary: Int,
-    wordBreakPolicy: LineWrap.WordBreakPolicy,
-    pointer0: Int,
-    pointer1: Int
-): Pair<String, Int> {
-    val wordWontFit = if (wordBreakPolicy == LineWrap.WordBreakPolicy.Hyphen)
-        substring(pointer0 until pointer1)
-    else ""
-
-    val shouldBreakWord = wordBreakPolicy == LineWrap.WordBreakPolicy.Hyphen &&
-            wordWontFit.length > boundary
-
-    val punctuation = if (shouldBreakWord)
-        wordWontFit.findAllPunctuations().lastOrNull { it.range.first <= boundary }
-    else null
-
-    val shouldTruncate = shouldBreakWord &&
-            (boundary - pointer0 + pos) > 2 && // can fit at least 3 characters
-            pos <= pointer0
-
-    val shouldAddTruncatedHyphen = shouldTruncate && punctuation == null
-
-    val end = when {
-        shouldTruncate && punctuation != null ->
-            pos + punctuation.range.first // break at any punctuation
-        shouldTruncate -> pos + boundary - wordBreakPolicy.text.length
-        (pointer1 - pos) < boundary -> pointer1
-        pos < pointer0 -> pointer0
-        else -> pos + boundary
-    }
-
-    val result =
-        substring(pos until end) + (if (shouldAddTruncatedHyphen) wordBreakPolicy.text else "")
-
-    val offset = when {
-        shouldAddTruncatedHyphen -> -1 // -1 if its truncated with hyphen
-        end < length && this[end] == ' ' -> 1 // add offset 1 if its a space
-        else -> 0
-    }
-    return Pair(result, pos + result.length + offset)
-}
-
 internal fun String.findAllPunctuations(): List<MatchResult> {
     return "[\\p{Punct}]+".toRegex().findAll(this).toList()
 }
